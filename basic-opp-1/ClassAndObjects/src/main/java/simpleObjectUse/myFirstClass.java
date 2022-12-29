5d4fba02a4bd19efcdaab2be49888b7eb7415f54
Pradip Budathoki <pradipbudathoki2018@gmail.com>
2022-12-28 22:09:16 -0600

    start of simple example of class and objects

---
+++ basic-opp-1/ClassAndObjects/src/main/java/simpleObjectUse/myFirstClass.java
@@ -0,0 +1,13 @@
+package simpleObjectUse;
+
+public class myFirstClass {
+    int numberFive = 5;
+
+    public static void main(String[] args){
+        myFirstClass dataStorage = new myFirstClass();
+        System.out.println(dataStorage.numberFive);
+    }
+
+
+}
+
