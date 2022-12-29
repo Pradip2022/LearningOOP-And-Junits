e47947daa9c49b7e13012e0aaeba06541005e152
Pradip Budathoki <pradipbudathoki2018@gmail.com>
2022-12-28 22:59:09 -0600

    basic use of methods and goes over how to use static and public methods

---
+++ basic-opp-1/ClassAndObjects/src/main/java/simpleMethods/simpleUseOfMethods.java
@@ -0,0 +1,30 @@
+package simpleMethods;
+
+public class simpleUseOfMethods {
+    static void myMethod(){
+        System.out.println("Hello World");
+    }
+
+    //Now I will be creating a static and public methods
+
+    static void myStaticMethod(){
+        System.out.println("I am a static method that can not be called by a object");
+
+    }
+
+    public void myPublicMethod(){
+        System.out.println("Public methods must be called by creating objects");
+    }
+
+    public static void main(String[] args){
+        myMethod();
+
+        //Calling the static method will always be like this
+        myStaticMethod();
+
+        //Calling the public method will allways be like this
+
+        simpleUseOfMethods allowsToCallPublicMethod = new simpleUseOfMethods();
+        allowsToCallPublicMethod.myPublicMethod();
+    }
+}
