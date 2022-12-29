5eaa312b89fe1dd42a32b6a5b3e3c34b4cd02ca0
Pradip Budathoki <pradipbudathoki2018@gmail.com>
2022-12-29 00:08:09 -0600

    added simple usage of multiple classes

---
+++ basic-opp-1/ClassAndObjects/src/main/java/simpleMulitpleClassUsage/Main.java
@@ -0,0 +1,10 @@
+package simpleMulitpleClassUsage;
+
+public class Main {
+    public void show(String showName){
+        System.out.println(showName);
+    }
+    public void episode(int maxEpisode){
+        System.out.println("The amount of episodes Naruto Shippuden has is "+ maxEpisode);
+    }
+}
---
+++ basic-opp-1/ClassAndObjects/src/main/java/simpleMulitpleClassUsage/UsingDataOfMain.java
@@ -0,0 +1,9 @@
+package simpleMulitpleClassUsage;
+
+class UsingDataOfMain {
+    public static void main(String[] args){
+        Main firstData = new Main();
+        firstData.show("naruto shippuden");
+        firstData.episode(500);
+    }
+}
