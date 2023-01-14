eb4880ce2af5f91d6eb52d53534ced5066ee789e
Pradip Budathoki <pradipbudathoki2018@gmail.com>
2023-01-14 00:45:42 -0600

    Inheritance

---
+++ basic-opp-1/ClassAndObjects/src/main/java/Inheritance/childLife.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class childLife extends parentMoney{
+    static int childMoney = 9000;
+
+    public static void main(String[] args) {
+        parentMoney pmoney = new parentMoney();
+        pmoney.parentMoney();
+        System.out.println("and after their death, I inherited all of the $" + pmoney.money );
+        System.out.println("As a result it brings the total to: $"+ (childMoney + pmoney.money));
+    }
+}
---
+++ basic-opp-1/ClassAndObjects/src/main/java/Inheritance/parentMoney.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+
+public class parentMoney {
+    protected int money = 1000000;
+    //kind of messed up but its a good example
+    public void parentMoney(){
+        System.out.println("My parents had " + money);
+    }
+}
