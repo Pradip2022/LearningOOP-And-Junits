6cd59a17c163e953c2ba103a02085c650e871293
Pradip Budathoki <pradipbudathoki2018@gmail.com>
2023-01-15 00:28:05 -0600

    Polymorphism

---
+++ basic-opp-1/ClassAndObjects/src/main/java/Polymorphism/Animal/Animal.java
@@ -0,0 +1,7 @@
+package Polymorphism.Animal;
+
+public class Animal {
+    public void animalSound(){
+        System.out.println("The animal makes a sound");
+    }
+}
---
+++ basic-opp-1/ClassAndObjects/src/main/java/Polymorphism/Animal/Dog.java
@@ -0,0 +1,7 @@
+package Polymorphism.Animal;
+
+public class Dog extends Animal {
+    public void animalSound(){
+        System.out.println("The dogs says: bow wow");
+    }
+}
